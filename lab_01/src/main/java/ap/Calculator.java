fe87e8f79fba0d6338b22a5d0b7754502cc9a09d
ILUXONCHIK <iluxon4ik@hotmail.com>
2017-03-02 09:46:00 +0000

    test: add tests for sum; fix failing tests

--- lab_01/src/main/java/ap/Calculator.java
+++ lab_01/src/main/java/ap/Calculator.java
@@ -29,6 +29,9 @@ public class Calculator {
     public Integer sum(Integer a, Integer b) {
         if (a == null)
             a = 0;
+        if (b == null)
+            b = 0;
+
         return a + b;
     }
 
--- lab_01/src/test/java/ap/TestCalculator.java
+++ lab_01/src/test/java/ap/TestCalculator.java
@@ -38,6 +38,17 @@ public class TestCalculator {
     }
 
     @Test
+    public void testConstructorInvalidNameNull() {
+        final String NAME = null;
+        try {
+            Calculator calc = new Calculator(NAME);
+            fail("Null name not refused");
+        } catch (IllegalArgumentException e) {
+            // success
+        }
+    }
+
+    @Test
     public void testConstructorInvalidNameLessThanTwo() {
         final String NAME = "T";
         try {
@@ -57,6 +68,79 @@ public class TestCalculator {
         } catch(IllegalArgumentException e) {
             // success
         }
+    }
+
+    @Test
+    public void testGetName() {
+        final String NAME = "Name";
+        Calculator calc = new Calculator(NAME);
+        assertEquals(calc.getName(), NAME, "Unexpected name");
+    }
+
+    @Test
+    public void testSumBothPositive() {
+        final int A = 1;
+        final int B = 2;
+        final int EXPECTED = 3;
+        Calculator calc = new Calculator("Calc");
+        int res = calc.sum(A, B);
+
+        assertEquals(res, EXPECTED);
+    }
+
+    @Test
+    public void testSumSecondArgNull() {
+        final int A = 1;
+        final Integer B = null;
+        final int EXPECTED = 1;
+        Calculator calc = new Calculator("Calc");
+        int res = calc.sum(A, B);
+
+        assertEquals(res, EXPECTED);
+    }
+
+    @Test
+    public void testSumFirstArgNull() {
+        final Integer A = null;
+        final Integer B = 1;
+        final int EXPECTED = 1;
+        Calculator calc = new Calculator("Calc");
+        int res = calc.sum(A, B);
+
+        assertEquals(res, EXPECTED);
+    }
+
+    @Test
+    public void testSumBothArgsNull() {
+        final Integer A = null;
+        final Integer B = null;
+        final int EXPECTED = 0;
+        Calculator calc = new Calculator("Calc");
+        int res = calc.sum(A, B);
+
+        assertEquals(res, EXPECTED);
+    }
+
+    @Test
+    public void testSumBothNegative() {
+        final int A = -1;
+        final int B = -2;
+        final int EXPECTED = -3;
+        Calculator calc = new Calculator("Calc");
+        int res = calc.sum(A, B);
+
+        assertEquals(res, EXPECTED);
+    }
+
+    @Test
+    public void testSumMixPositiveNegative() {
+        final int A = -1;
+        final int B = 2;
+        final int EXPECTED = 1;
+        Calculator calc = new Calculator("Calc");
+        int res = calc.sum(A, B);
+
+        assertEquals(res, EXPECTED);
     }
 
 }
